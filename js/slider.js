384e3cfbdd41753f0d1170bab68e00bd67de4dda
Final <Final@Finals-MBP.hitronhub.home>
2018-10-23 11:43:28 -0700

    Update New One

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+
+//  $("#slideshow .inner > div:gt(0)").hide();
+
+// setInterval(function() {
+//   $('#slideshow .inner > div:first')
+//     .fadeOut(1000)
+//     .next()
+//     .fadeIn(1000)
+//     .end()
+//     .appendTo('#slideshow .inner');
+// }, 3000);
