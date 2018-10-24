6172a07e45758b2ebb6b2f3036b0522bbc1c1991
Final <Final@Finals-MBP.hitronhub.home>
2018-10-24 14:56:13 -0700

    Uploaded CloseButton.js

---
+++ js/CloseButton.js
@@ -0,0 +1,12 @@
+
+function CloseBox1(){
+
+	var x = document.getElementsByClassName("navleft");
+	console.log(x);
+	x[0].style.display="none";
+
+}
+function CloseBox(){
+	var x = document.getElementsByClassName("sidenav");
+	x[0].style.display="none";
+}
